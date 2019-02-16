86992bc6ed6bf272eab992cc593958324075b7e2
Marco Faedo <marco.faedo@smacc.io>
2019-02-16 12:28:11 +0100

    adding utils

---
+++ johnny_deep/utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def get_cost_value(Y_hat, Y):
+    # number of examples
+    m = Y_hat.shape[1]
+    # calculation of the cost according to the formula
+    cost = -1 / m * (np.dot(Y, np.log(Y_hat).T) + np.dot(1 - Y, np.log(1 - Y_hat).T))
+    return np.squeeze(cost)
+
+
+def convert_prob_into_class(probs):
+    # an auxiliary function that converts probability into class
+    probs_ = np.copy(probs)
+    probs_[probs_ > 0.5] = 1
+    probs_[probs_ <= 0.5] = 0
+    return np.squeeze(probs_)
